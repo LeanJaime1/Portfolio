50a33b04ae387484800c846ca50c8ba9e8b31266
Leandro Jaime <leandrojaime17@gmail.com>
2026-09-02 16:20:34 -0300

    Corrijo SVG

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -293,22 +293,7 @@ const Contact = () => {
                 <span>
                   {status.submitting ? "Enviando..." : "Enviar mensaje"}
                 </span>
-                {!status.submitting && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    className="w-4 h-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
-                )}
+                {!status.submitting}
               </motion.button>
             </form>
           </motion.div>
